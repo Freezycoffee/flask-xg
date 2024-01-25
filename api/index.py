1bdf79b85dc4cf4b76e7590e74c9fe6271e2bc87
Khofi Alfian <136216871+Freezycoffee@users.noreply.github.com>
2024-01-25 20:30:30 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -11,7 +11,7 @@ def hello_world():
 @app.route("/data",methods=['POST','GET'])
 def hello():
     data = request.get_json(force=True)
-    df = [data['player_pos'],data['distance_to_l'],data['distance_to_r'],data['angle'],data['shot_technique'],data['player_in_the_way'],data['situation'],data['pass_type'],data['inside_pen_box'],data['inside_6_box'],data['body_part'],data['big_chances'],data['on_target']]
+    df = dict(data)
     url = "https://alfiankhofi.ap-south-1.modelbit.com/v1/xg_predict/latest"
     headers = {
     'Content-Type': 'application/json'}
@@ -21,10 +21,6 @@ def hello():
     response = requests.post(url, headers=headers, json=data)
     response_json = response.json()
     return json.dumps(response_json, indent=4)
-
-@app.route("/favicon.ico")
-def favicon():
-    return url_for('static',filename='clipboard.png')
 
 if __name__ == "__main__":
     app.run()
